5b4c2fb0938c97b68732dddfd99f124c303372b4
Luca Quaresima <luca.quaresima@studenti.univr.it>
2024-12-11 14:57:09 +0100

    Updated Main.java package 10

--- src/package10/Main.java
+++ src/package10/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args){
         Draw h=new H(), e=new E(), l=new L(),o=new O(),star=new Star();
         Draw hello=new HorizontalDraw(star,h,e,l,l,o,star);
-        System.out.println(hello);
         Draw frame =new FrameDraw(hello);
-        System.out.println(frame);
+        System.out.println(new HorizontalDraw(hello,frame));
     }
 }
